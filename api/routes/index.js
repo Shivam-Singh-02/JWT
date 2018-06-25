602ba779672e13d308ecee2e060283ebe0839354
Shivam-Singh <shivamsingh_15123@aitpune.edu.in>
2018-06-25 15:54:56 +0530

    Added comments

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,18 +1,25 @@
+// Require the needed libraries
+
 const Express = require('express')
 const jwt = require ('jsonwebtoken')
 const path = "4040"
-const app = Express()
+
+
+//Declare constants used
+//dafault key used for creating token
 const secretKey = "im batman"
 
+//Make instance of Express
+const app = Express()
 
-
+// A Sample GET route
 app.get('/', (req, res) => {
   res.json({
     message : "Welcome to API"
   })
 })
 
-
+// The protected route that uses the validation and middle function validateUser
 app.post('/', validateUser, (req, res) => {
   jwt.verify(req.token, secretKey, (err, authData) => {
     if (err) {
@@ -25,6 +32,7 @@ app.post('/', validateUser, (req, res) => {
 })
 
 
+//The login route that produces the token to be used for login
 app.post('/login', (req, res) => {
   const user = {
     id : 1,
@@ -42,7 +50,7 @@ app.post('/login', (req, res) => {
 })
 
 
-// Validate User
+// The middleware function that checks for the token and returns error if no Header is included
 function validateUser(req, res, next) {
 
   const bearerHeader = req.headers['authorization']
